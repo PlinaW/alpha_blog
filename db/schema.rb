b0f8a8a061e4ffcbc17fc2c7e5804a9cf4afc7ec
PlinaW <plinski.wojciech@gmail.com>
2022-12-01 21:43:05 +0100

    Add users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_175829) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_203838) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
