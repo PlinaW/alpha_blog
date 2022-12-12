98ff28857c68777d99d3af493381ac016ae7bc7b
PlinaW <plinski.wojciech@gmail.com>
2022-12-12 20:18:08 +0100

    add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class  UsersController < ApplicationController
   end
 
   def create
+      @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the AlphaBlog #{@user.username}! User was created successfully."
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
